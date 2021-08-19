af1fc4c148d88c800f65c19e802cd8671096577c
Leo Carvalho <carvalho.csleo@gmail.com>
2021-08-18 21:35:47 -0300

    Aula inicial de React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form action="">
+        <label htmlFor="nome">Nome</label>
+        <input type="text" name="nome" id="nome" />
+
+        <br /><br />
+
+        <label htmlFor="email">E-mail</label>
+        <input type="email" name="email" id="email" />
+
+        <br /><br />
+
+        <label htmlFor="senha">Senha</label>
+        <input type="password" name="senha" id="senha" />
+
+        <br /><br />
+
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
